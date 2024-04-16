e78de2484b2c778b0b4ecd6cb7b650764c38a753
shukriddinovs <sshukriddinov524@gmail.com>
2024-04-16 14:09:30 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,6 @@
+let name = prompt("Ismingizni kiriting")
+let surname = prompt("Familyangizni kiriting")
+let age = prompt("Yoshingizni kiriting")
+console.log(name);
+console.log(surname);
+console.log(age);
